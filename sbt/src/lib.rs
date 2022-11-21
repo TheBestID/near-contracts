b33b5229874dd5a7f76e91cb553a78b8cdd418de
andzh1 <andrey.zhizhelev@yandex.ru>
2022-11-21 23:29:21 +0300

    Fixed HashMap issues in sbt

--- sbt/src/lib.rs
+++ sbt/src/lib.rs
@@ -50,13 +50,17 @@ impl SBT {
 
     pub fn claim(&mut self, new_git_hash: &MyHash, new_email_hash: &MyHash) {
         let msg_sender = env::signer_account_id();
+        require!(
+            self.soul_id_of_account_.contains_key(&msg_sender),
+            "Soul must exist to be able to burn it"
+        );
         let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
         require!(
-            self.minted_not_claimed[&msg_sender_soul_id],
+            self.minted_not_claimed.contains_key(&msg_sender_soul_id) && self.minted_not_claimed[&msg_sender_soul_id],
             "Soul must be minted to be able to claim it"
         );
         self.souls_.get_mut(&msg_sender_soul_id).unwrap().email_hash = *new_email_hash;
-        self.souls_.get_mut(&msg_sender_soul_id).unwrap().git_hash = *new_git_hash; // TODO: Is it correct? Does it store value, not pointer?
+        self.souls_.get_mut(&msg_sender_soul_id).unwrap().git_hash = *new_git_hash; 
         *self
             .account_of_soul_id
             .get_mut(&msg_sender_soul_id)
@@ -65,29 +69,42 @@ impl SBT {
     }
 
     pub fn get_user_id(&self, account: &AccountId) -> u128 {
+        require!(self.soul_id_of_account_.contains_key(account), "No user found");
         self.soul_id_of_account_[account]
     }
 
     pub fn burn(&mut self) {
         let msg_sender = env::signer_account_id();
-        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
         require!(
-            self.account_of_soul_id[&msg_sender_soul_id] == msg_sender,
+            self.soul_id_of_account_.contains_key(&msg_sender),
             "Soul must exist to be able to burn it"
         );
-
+        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
+        require!(
+            self.souls_.contains_key(&msg_sender_soul_id),
+            "Soul must exist to be able to burn it"
+        );
         self.soul_id_of_account_.remove(&msg_sender);
         self.account_of_soul_id.remove(&msg_sender_soul_id);
         self.souls_.remove(&msg_sender_soul_id);
     }
 
     pub fn has_soul(&self, account: &AccountId) -> bool {
-        let account_soul_id = self.soul_id_of_account_[account];
-        self.account_of_soul_id[&account_soul_id] != *account
+        if !self.soul_id_of_account_.contains_key(account) {
+            return false;
+        }
+        if !self.souls_.contains_key(&self.soul_id_of_account_[account]) {
+            return false;
+        }
+        true
     }
 
     pub fn ping(&self) -> bool {
         true
+    }
+
+    pub fn ping_string(&self) -> String {
+        "I'm okey".to_string()
     }
 
     pub fn get_hashed_data(&self) -> HashedData {
@@ -110,7 +127,6 @@ impl SBT {
 mod tests {
     use super::*;
 
-    const HELLO_NEAR: &str = "beneficiary";
 
     #[test]
     fn initializes() {
