341a9b579321aa489ffcafe596eb607ddd5be30c
andzh1 <andrey.zhizhelev@yandex.ru>
2022-11-28 17:25:16 +0300

    Fixed bugs

---
+++ sbt/call.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo ">> Calling contract"
+
+near call sbt.soul_dev.testnet mint '{"new_id": 1, "account": "andzhi.testnet"}' --accountId sbt.soul_dev.testnet
+near call sbt.soul_dev.testnet claim '{"new_git_hash": "0xcccccccccccccccccccccccccccccc", "new_email_hash": "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"}' --accountId andzhi.testnet
+near call sbt.soul_dev.testnet burn '{}' --accountId andzhi.testnet
+near call sbt.soul_dev.testnet get_account_id '{"user_id": 1}' --accountId sbt.soul_dev.testnet 
+near call sbt.soul_dev.testnet get_user_id '{"account": "andzhi.testnet"}' --accountId sbt.soul_dev.testnet
+near call sbt.soul_dev.testnet has_soul '{"account" : "andzhi.testnet"}' --accountId andzhi.testnet
+near call sbt.soul_dev.testnet get_hashed_data '{}' --accountId andzhi.testnet
+near call sbt.soul_dev.testnet soul_is_minted_not_claimed '{"account": "andzhi.testnet"}' --accountId sbt.soul_dev.testnet
--- sbt/src/lib.rs
+++ sbt/src/lib.rs
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, require, AccountId};
+use near_sdk::{env, log, near_bindgen, require, AccountId};
 use std::collections::HashMap;
+use std::str;
 
 type MyHash = [u8; 32];
-type HashedData = [MyHash; 2];
+type HashedData = [String; 2];
 
 #[derive(Default, BorshDeserialize, BorshSerialize, Copy, Clone)]
 pub struct Soul {
@@ -25,7 +26,7 @@ pub struct SBT {
 #[near_bindgen]
 impl SBT {
     #[init]
-    #[private] 
+    #[private]
     pub fn init() -> Self {
         Self {
             souls_: HashMap::new(),
@@ -35,69 +36,82 @@ impl SBT {
         }
     }
 
-    pub fn mint(&mut self, new_id: u128, account: &AccountId) {
-        require!(!self.souls_.contains_key(&new_id), "Soul exists");
+    pub fn mint(&mut self, new_id: u128, account: AccountId) {
+        require!(!self.has_soul(&account), "Soul exists");
         require!(
-            !self.minted_not_claimed.contains_key(&new_id),
+            !self.soul_is_minted_not_claimed(&account),
             "Soul is already minted"
         );
         require!(
             env::signer_account_id() == env::current_account_id(),
             "Only this contract can mint SBT"
         );
-        *self.soul_id_of_account_.get_mut(account).unwrap() = new_id;
-        *self.minted_not_claimed.get_mut(&new_id).unwrap() = true;
+        self.soul_id_of_account_.insert(account, new_id);
+        self.minted_not_claimed.insert(new_id, true);
     }
 
-    pub fn claim(&mut self, new_git_hash: &MyHash, new_email_hash: &MyHash) {
+    pub fn claim(&mut self, new_git_hash: &String, new_email_hash: &String) {
         let msg_sender = env::signer_account_id();
         require!(
-            self.soul_id_of_account_.contains_key(&msg_sender),
-            "Soul must exist to be able to burn it"
-        );
-        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
-        require!(
-            self.minted_not_claimed.contains_key(&msg_sender_soul_id)
-                && self.minted_not_claimed[&msg_sender_soul_id],
+            self.soul_is_minted_not_claimed(&msg_sender),
             "Soul must be minted to be able to claim it"
         );
-        self.souls_.get_mut(&msg_sender_soul_id).unwrap().email_hash = *new_email_hash;
-        self.souls_.get_mut(&msg_sender_soul_id).unwrap().git_hash = *new_git_hash;
-        *self
-            .account_of_soul_id
-            .get_mut(&msg_sender_soul_id)
-            .unwrap() = msg_sender;
-        self.minted_not_claimed.remove(&msg_sender_soul_id);
+        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
+        log!(
+            "New git hash is {}, new email hash is {}",
+            new_git_hash,
+            new_email_hash
+        );
+        require!(
+            new_git_hash.chars().count() == 32,
+            "Given hash must be 32 character length"
+        );
+        require!(
+            new_email_hash.chars().count() == 32,
+            "Given hash must be 32 character length"
+        );
+        let mut hashed_git: MyHash = [0; 32];
+        for i in 0..32 {
+            hashed_git[i] = new_git_hash.as_bytes()[i];
+        }
+        let mut hashed_email: MyHash = [0; 32];
+        for i in 0..32 {
+            hashed_email[i] = new_email_hash.as_bytes()[i];
+        }
+        
+        let current_soul = Soul {
+            soul_id: msg_sender_soul_id,
+            git_hash: hashed_git,
+            email_hash: hashed_email,
+        };
+        self.souls_.remove(&msg_sender_soul_id);
+        self.account_of_soul_id.remove(&msg_sender_soul_id);
+        self.souls_.insert(msg_sender_soul_id, current_soul);
+        self.account_of_soul_id
+            .insert(msg_sender_soul_id, msg_sender);
     }
 
     pub fn get_user_id(&self, account: &AccountId) -> u128 {
-        require!(
-            self.soul_id_of_account_.contains_key(account),
-            "No user found"
-        );
+        require!(self.has_soul(account), "No user found");
         self.soul_id_of_account_[account]
     }
 
     pub fn get_account_id(&self, user_id: u128) -> AccountId {
         require!(
-            self.souls_.contains_key(&user_id),
+            self.account_of_soul_id.contains_key(&user_id)
+                && self.has_soul(&self.account_of_soul_id[&user_id]),
             "No user found"
         );
         self.account_of_soul_id[&user_id].clone()
     }
 
-
     pub fn burn(&mut self) {
         let msg_sender = env::signer_account_id();
         require!(
-            self.soul_id_of_account_.contains_key(&msg_sender),
+            self.has_soul(&msg_sender),
             "Soul must exist to be able to burn it"
         );
         let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
-        require!(
-            self.souls_.contains_key(&msg_sender_soul_id),
-            "Soul must exist to be able to burn it"
-        );
         self.soul_id_of_account_.remove(&msg_sender);
         self.account_of_soul_id.remove(&msg_sender_soul_id);
         self.souls_.remove(&msg_sender_soul_id);
@@ -113,6 +127,17 @@ impl SBT {
         true
     }
 
+    pub fn soul_is_minted_not_claimed(&self, account: &AccountId) -> bool {
+        if !self.soul_id_of_account_.contains_key(&account) {
+            return false;
+        }
+        let msg_sender_soul_id = self.soul_id_of_account_[&account];
+        if !self.minted_not_claimed.contains_key(&msg_sender_soul_id) {
+            return false;
+        }
+        self.minted_not_claimed[&msg_sender_soul_id]
+    }
+
     pub fn ping(&self) -> bool {
         true
     }
@@ -123,15 +148,14 @@ impl SBT {
 
     pub fn get_hashed_data(&self) -> HashedData {
         let msg_sender = env::signer_account_id();
-        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
         require!(
-            self.account_of_soul_id[&msg_sender_soul_id] == msg_sender,
+            self.has_soul(&msg_sender),
             "Soul must exist to get it's data"
         );
-        let mut users_data = [[0; 32]; 2];
-
-        users_data[0] = self.souls_[&msg_sender_soul_id].git_hash;
-        users_data[1] = self.souls_[&msg_sender_soul_id].email_hash;
-        users_data
+        let msg_sender_soul_id = self.soul_id_of_account_[&msg_sender];
+        [
+            String::from_utf8(self.souls_[&msg_sender_soul_id].git_hash.to_vec()).unwrap(),
+            String::from_utf8(self.souls_[&msg_sender_soul_id].email_hash.to_vec()).unwrap(),
+        ]
     }
 }
